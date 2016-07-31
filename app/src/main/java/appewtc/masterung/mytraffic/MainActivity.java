ebb2d474dfcd6f16038429089ce68ca71b13f448
masterUNG <phrombutr@gmail.com>
2016-07-31 10:59:02 +0700

    Intent to WebView

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -1,10 +1,18 @@
 package appewtc.masterung.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private String urlYoutubeString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickAboutMe(View view) {
 
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.effect_btn_long);
+        mediaPlayer.start();
+
+        //Intent to WebView
+        urlYoutubeString = "https://youtu.be/AFmWqLIqDZA";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
     }
 
 }   // Main Class นี่คือ คลาสหลัก
